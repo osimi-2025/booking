358e1bee564ae84f944942ec2e3e454035cf8b4d
osimi-2025 <lyceum.osimi1@gmail.com>
2025-08-11 10:17:54 +0500

    new room class

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BookingHotel;
+
+List<Room> rooms = new List<Room>();
+while (true)
+{
+    int command = int.Parse(Console.ReadLine());
+    if (command == 1)
+    {
+        int id = int.Parse(Console.ReadLine());
+        string type = Console.ReadLine();
+        int floor = int.Parse(Console.ReadLine());
+        int price = int.Parse(Console.ReadLine());
+        int capacity = int.Parse(Console.ReadLine());
+        string status = Console.ReadLine();
+        string description = Console.ReadLine();
+
+        Room room = new Room(id, type, floor, price, capacity, status, description);
+        rooms.Add(room);
+        Console.WriteLine("Created new Room!");
+    } else
+    {
+        foreach (Room room in rooms)
+        {
+            room.GetInfo();
+        }
+    }
+}
---
+++ Room.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingHotel
+{
+    internal class Room
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public int Floor { get; set; }
+        public int Price { get; set; }
+        public int Capacity { get; set; }
+        public string Status { get; set; }
+
+        public string Description { get; set; }
+
+        public Room(int id, string type, int floor, int price, int capacity, string status, string description)
+        {
+            Id = id;
+            Type = type;
+            Floor = floor;
+            Price = price;
+            Capacity = capacity;
+            Status = status;
+            Description = description;
+        }
+
+        public void GetInfo()
+        {
+
+            Console.WriteLine($"ID: {Id}, Type: {Type}");
+        }
+    }
+}
